1ae34f63bae18e377683d7400526d1401ce3106e
Mohamed <mmuseaden@gmail.com>
2022-03-24 00:16:57 +0000

    first commit

---
+++ initials.py
@@ -0,0 +1,23 @@
+# https://www.codecademy.com/courses/learn-python-3/projects/python-block-letters
+name = """
+M     M   M     M      A            
+MM   MM   MM   MM    A   A          
+MM   MM   MM   MM   A     A         
+M  M  M   M  M  M   AAAAAAAA        
+M     M   M     M   A      A        
+M     M   M     M   A      A        
+M     M , M     M , A      A        
+"""
+print(name)
+
+f_name = "Mohamed"
+l_name = "Muse Aden"
+comment = "I " + f_name + " " + l_name + " just recently sparked an interest for computer science"
+print(comment)
+print(" M   M  M   M    A   ")
+print(" MM MM  MM MM   A A  ")
+print(" MM MM  MM MM  A   A ")
+print(" M M M  M M M  AAAAA ")
+print(" M   M  M   M  A   A ")
+print(" M   M  M   M  A   A ")
+print(" M   M  M   M  A   A ")
